a9c9008f8252c1514239dd84ed6a5e2bbf06668c
QuentinParsy <parsyquentin@gmail.com>
2022-05-12 23:54:14 +0200

    sounds added (pls stop my head...)

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,9 +2,30 @@
 let computerChoiceDisplay = document.getElementById('computer')
 let userChoiceDisplay = document.getElementById('user')
 let resultDisplay = document.getElementById('result')
-let possibleChoices = document.querySelectorAll('button')
+let possibleChoices = document.getElementsByName('playGame')
 let choice
 let result
+
+// audio correct de bouton
+let playWin = () => {
+  new Audio('assets/img/nice.mp3').play()
+}
+
+// audio incorrect de bouton
+let playLose = () => {
+  new Audio('assets/img/wrong.mp3').play()
+}
+
+//audio égalité de bouton
+let playDraw = () => {
+  new Audio('assets/img/draw.mp3').play()
+}
+
+//bouton musique
+let playMusic = () => {
+  new Audio('assets/img/.mp3').play()
+}
+
 
 // Génération de choix par l'ordinateur
 let generateComputerChoice = () => {
@@ -25,32 +46,40 @@ let generateComputerChoice = () => {
   let = getResult = () => {
     if (computerChoice === choice) {
       result = 'Draw'
+      playDraw()
     }
     if (computerChoice === 'Rock ぐう' && choice === "Paper ぱあ") {
       result = 'You win'
+      playWin()
     }
     if (computerChoice === 'Rock ぐう' && choice === "Scissors ちょき") {
       result = 'You lost'
+      playLose()
     }
     if (computerChoice === 'Paper ぱあ' && choice === "Scissors ちょき") {
       result = 'You win'
+      playWin()
     }
     if (computerChoice === 'Paper ぱあ' && choice === 'Rock ぐう') {
       result = 'You lose'
+      playLose()
     }
     if (computerChoice === 'Scissors ちょき' && choice === 'Rock ぐう') {
       result = 'You win'
+      playWin()
     }
     if (computerChoice === 'Scissors ちょき' && choice === "Paper ぱあ") {
       result = 'You lose'
+      playLose()
     }
     resultDisplay.innerHTML = result
   }
 
-
+// Clic bouton
 possibleChoices.forEach(possibleChoices=> possibleChoices.addEventListener('click', (e) => {
     choice = e.target.id
     userChoiceDisplay.innerHTML = (`You have chosen : ${choice}`)
     generateComputerChoice()
     getResult()
 }))
+
